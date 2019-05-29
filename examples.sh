7066a2d9874130e6ecbd83ab5c9df7690b8eb702
Lisandro Fernandez <kelechul@gmail.com>
2019-05-29 20:35:24 -0300

    Fix example

--- examples.sh
+++ examples.sh
@@ -8,9 +8,9 @@ curl -v localhost:8080/api/v1/reserves/1 | jq '.'
 # create natural reserve
 curl -v -X POST -H 'Content-Type: application/json' -d '{"name": "Hyde Park", "regionId": 1}' localhost:8080/api/v1/reserves | jq '.'
 # update natural reserve
-curl -v -X POST -H 'Content-Type: application/json' -d '{"name": "Central Park", "regionId": 1}' localhost:8080/api/v1/reserves/1 | jq '.'
+curl -v -X PUT -H 'Content-Type: application/json' -d '{"name": "Central Park", "regionId": 1}' localhost:8080/api/v1/reserves/2 | jq '.'
 # delete natural reserve
-curl -v -X DELETE localhost:8080/api/v1/reserves/1 | jq '.'
+curl -v -X DELETE localhost:8080/api/v1/reserves/2 | jq '.'
 
 # get chances by month
 curl -v localhost:8080/api/v1/chances?date=2019-06-01 | jq '.'
