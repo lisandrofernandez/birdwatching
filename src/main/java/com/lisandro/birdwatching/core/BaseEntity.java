eeb67605aa039441f74fca4014f599060386989e
Lisandro Fernandez <kelechul@gmail.com>
2020-04-07 11:22:18 +0200

    Make BaseEntity class abstract

--- src/main/java/com/lisandro/birdwatching/core/BaseEntity.java
+++ src/main/java/com/lisandro/birdwatching/core/BaseEntity.java
@@ -29,7 +29,7 @@ import javax.persistence.MappedSuperclass;
 import javax.persistence.Version;
 
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
