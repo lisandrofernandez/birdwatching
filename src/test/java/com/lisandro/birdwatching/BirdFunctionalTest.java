55e4a6ce71cda05065a5ff8847c64eace20c9242
Lisandro Fernandez <kelechul@gmail.com>
2020-04-20 16:33:38 +0200

    Add bird functional tests

---
+++ src/test/java/com/lisandro/birdwatching/BirdFunctionalTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2020 Lisandro Fernandez
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.lisandro.birdwatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.lisandro.birdwatching.controller.BirdController.BASE_URL;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Functional tests of bird use cases.
+ *
+ * @author Lisandro Fernandez
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class BirdFunctionalTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getBirdTuplesTest() throws Exception {
+        // when
+        mockMvc.perform(get(BASE_URL).accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk()) // then
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(content().json(
+                      "[{'id': 1, 'name': 'Falcon'},"
+                    + " {'id': 2, 'name': 'Spoonbill'},"
+                    + " {'id': 3, 'name': 'Goose'},"
+                    + " {'id': 4, 'name': 'Great bustard'}]", true)
+            );
+    }
+}
